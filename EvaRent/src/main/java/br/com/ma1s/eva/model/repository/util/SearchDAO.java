9275341651a811a3330d1c9c1129e671ce28f574
Administrador <Administrador@STFCLOC023703-L.stefanini.dom>
2015-02-26 08:57:28 -0300

    - Alteração de responsabilidade de classes que efetuam as buscas por formulários genéricos

--- EvaRent/src/main/java/br/com/ma1s/eva/model/repository/util/SearchDAO.java
+++ EvaRent/src/main/java/br/com/ma1s/eva/model/repository/util/SearchDAO.java
@@ -6,7 +6,6 @@
 package br.com.ma1s.eva.model.repository.util;
 
 import br.com.ma1s.eva.web.util.SearchParam;
-import br.com.ma1s.eva.model.repository.query.SearchQueryTranslator;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -22,30 +21,7 @@ public class SearchDAO {
     
     @Inject private EntityManager manager;
     
-    private String getQuery(SearchQueryTranslator translator, List<SearchParam> params) {
-        
-        final StringBuilder sb = new StringBuilder();
-        sb.append("SELECT e FROM ")
-          .append(translator.getType().getSimpleName())
-          .append(" e WHERE ");
-        
-        for (int i = 0; i < params.size(); i++) {
-            final SearchParam param = params.get(i);
-            
-            if (i > 0) 
-                sb.append(" AND ");
-            
-            sb.append(translator.getAttribute(param.getField()))
-              .append(" ")
-              .append(param.getCondition().getSql())
-              .append(i+1);
-            
-        }
-        return sb.toString();
-    }
-    
-    public List find(SearchQueryTranslator translator, List<SearchParam> params) {
-        final String sql = getQuery(translator, params);
+    public List find(String sql, List<SearchParam> params) {
         final Query query = manager.createQuery(sql);
         
         for (int i = 0; i < params.size(); i++) {
@@ -54,9 +30,7 @@ public class SearchDAO {
         return query.getResultList();
     }
     
-    public List find(SearchQueryTranslator translator, List<SearchParam> params,
-                     int first, int max) {
-        final String sql = getQuery(translator, params);
+    public List find(String sql, List<SearchParam> params, int first, int max) {
         final Query query = manager.createQuery(sql);
         
         for (int i = 0; i < params.size(); i++) {
--- EvaRent/src/main/java/br/com/ma1s/eva/service/GenericSearchService.java
+++ EvaRent/src/main/java/br/com/ma1s/eva/service/GenericSearchService.java
@@ -25,13 +25,37 @@ public class GenericSearchService<T> implements Serializable {
     public List<T> find(SearchQueryTranslator translator, 
                             List<SearchParam> params) {
         
-        return dao.find(translator, params);
+        final String query = getQuery(translator, params);
+        return dao.find(query, params);
     }
     
     public List<T> find(SearchQueryTranslator translator, 
                             List<SearchParam> params,
                             int first, int max) {
+     
+        final String query = getQuery(translator, params);
+        return dao.find(query, params, first, max);
+    }
+    
+    private String getQuery(SearchQueryTranslator translator, List<SearchParam> params) {
         
-        return dao.find(translator, params, first, max);
+        final StringBuilder sb = new StringBuilder();
+        sb.append("SELECT e FROM ")
+          .append(translator.getType().getSimpleName())
+          .append(" e WHERE ");
+        
+        for (int i = 0; i < params.size(); i++) {
+            final SearchParam param = params.get(i);
+            
+            if (i > 0) 
+                sb.append(" AND ");
+            
+            sb.append(translator.getAttribute(param.getField()))
+              .append(" ")
+              .append(param.getCondition().getSql())
+              .append(i+1);
+            
+        }
+        return sb.toString();
     }
 }
