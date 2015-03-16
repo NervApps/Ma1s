527f46d81765d303e39d23808f0169830069ceec
Administrador <Administrador@STFCLOC023703-L.stefanini.dom>
2015-03-16 16:22:54 -0300

    - Alteração do nome do bean para condizer melhor com sua funcionalidade

--- EvaRent/src/main/java/br/com/ma1s/eva/web/beans/common/TemplateBean.java
+++ EvaRent/src/main/java/br/com/ma1s/eva/web/beans/common/TemplateBean.java
@@ -7,27 +7,18 @@ package br.com.ma1s.eva.web.beans.common;
 
 import br.com.ma1s.eva.model.PaymentRegister;
 import br.com.ma1s.eva.service.PaymentService;
-import java.io.Serializable;
 import java.util.List;
-import javax.annotation.PostConstruct;
-import javax.faces.bean.SessionScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import lombok.Getter;
 
 /**
  *
  * @author Vitor
  */
-@Named @SessionScoped
-public class IndexBean extends ManagedBean implements Serializable {
-    @Getter private int pendentPercentual;
+@Named @RequestScoped
+public class TemplateBean extends ManagedBean {
     @Inject private PaymentService service;
-    
-    @PostConstruct
-    public void init() {
-        loadPendentPercentual();
-    }
     
     public long getPendents() {
         return service.countMonthPendents();
@@ -37,7 +28,7 @@ public class IndexBean extends ManagedBean implements Serializable {
         return service.getFirstPendents();
     }
     
-    public void loadPendentPercentual() {
-        pendentPercentual = service.getPendentPercentual();
+    public int getPendentPercentual() {
+        return service.getPendentPercentual();
     }
 }
